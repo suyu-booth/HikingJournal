165adae239365887df93fd54d765ce2ecf90070d
Su Yu <suyu@chicagobooth.edu>
2025-05-13 21:51:19 -0400

    Initial Commit

---
+++ HikingJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HikingJournal
+//
+//  Created by Su Yu on 5/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
